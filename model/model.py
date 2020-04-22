458e1acdcb0e3670b1af0fe9e1aff02ee564dfc5
carynJohansen <caryn@weareher.com>
2020-04-22 10:33:25 -0700

    toy example

---
+++ model/model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def scammerScore(userID):
+    """Calculates the probability that the specified user is a scammer."""
+    res = getMoreStuff(userID)
+    return res
+
+
+
+def getMoreStuff(userID):
+	"""More stuff"""
+	return 0.42
---
+++ model/test/test.py
@@ -0,0 +1,5 @@
+
+from ..model import scammerScore
+
+if __name__ == '__main__':
+    print(scammerScore(123))
